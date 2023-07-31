c0a36cbb2e21c11850dc19f0051ddcda38cafd6d
Vasily Sokolov <vasilysokolov@mail.ru>
2023-07-31 17:45:54 +0300

    Добавьте файлы проекта.

---
+++ CapturingTheTitleOfWebPage.cpp
@@ -0,0 +1,12 @@
+﻿// CapturingTheTitleOfWebPage.cpp: определяет точку входа для приложения.
+//
+
+#include "CapturingTheTitleOfWebPage.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
