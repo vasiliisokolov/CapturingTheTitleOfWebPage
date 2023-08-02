890d86381833e77dfaca4b0e057a7fa6891886f4
Vasily Sokolov <vasilysokolov@mail.ru>
2023-08-02 20:59:03 +0300

    fast

--- CapturingTheTitleOfWebPage.cpp
+++ CapturingTheTitleOfWebPage.cpp
@@ -3,10 +3,21 @@
 
 #include "CapturingTheTitleOfWebPage.h"
 
-using namespace std;
-
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	
+	cpr::Response r = cpr::Get(cpr::Url{"https://httpbin.org/html"}, 
+		cpr::Header{{"Accept", "text/html"}});
+
+	int s = r.text.find("<h1>");
+	std::cout << s << std::endl;
+	int f = r.text.rfind("<h1>");
+	std::cout << f << std::endl;
+
+	for (int i = s; i < f; i++)
+	{
+		std::cout << r.text[i];
+	}
+		
+
 }
