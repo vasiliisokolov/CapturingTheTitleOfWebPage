d5facca4c4fd46a9c6ddc34112dc1386ba2a7f4b
Vasily Sokolov <vasilysokolov@mail.ru>
2023-08-02 21:22:23 +0300

    Done!

--- CapturingTheTitleOfWebPage.cpp
+++ CapturingTheTitleOfWebPage.cpp
@@ -8,13 +8,13 @@ int main()
 	
 	cpr::Response r = cpr::Get(cpr::Url{"https://httpbin.org/html"}, 
 		cpr::Header{{"Accept", "text/html"}});
-
+	//std::cout << r.text << std::endl;
 	int s = r.text.find("<h1>");
-	std::cout << s << std::endl;
-	int f = r.text.rfind("<h1>");
-	std::cout << f << std::endl;
+	//std::cout << s << std::endl;
+	int f = r.text.find("</h1>", s+5);
+	//std::cout << f << std::endl;
 
-	for (int i = s; i < f; i++)
+	for (int i = s+4; i < f; i++)
 	{
 		std::cout << r.text[i];
 	}
--- CapturingTheTitleOfWebPage.h
+++ CapturingTheTitleOfWebPage.h
@@ -2,5 +2,6 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <cpr/cpr.h>
 
